a06cefdda5d117d9a1fc54f1248808bc438f8368
Amitesh Krishna <anantk124@gmail.com>
2020-12-12 15:55:46 +0530

    AAAAAAAAAAAAAA

---
+++ Enemy.js
@@ -0,0 +1,34 @@
+class Enemy{
+	constructor(x,y){
+		this.sprite = createSprite(x,y,30,30);
+		this.sprite.addImage("right",enemyImg1);
+		this.sprite.addImage("left",enemyImg2);
+		this.sprite.scale = 0.1;
+		this.x = x;
+		this.y = y;
+		this.look = "right";
+	}
+
+	kill(){
+		this.sprite.destroy();
+	}
+
+	move(){
+		if(this.sprite.x > this.x - 100 && this.sprite.x < this.x + 100){
+			if(this.sprite.x == this.x + 100){
+				this.look = "left";
+			}
+			if(this.sprite.x == this.x - 100){
+				this.look = "right";
+			}
+		}
+		if(this.look == "right"){
+			this.sprite.changeImage("right");
+			this.sprite.x += 5;
+		}
+		if(this.look == "left"){
+			this.sprite.changeImage("left");
+			this.sprite.x -= 5;
+		}
+	}
+}
--- sketch.js
+++ sketch.js
@@ -2,16 +2,24 @@ var Mario;
 var Ground;
 var gameState = 1;
 var obs = [];
+var enemies = [];
+var fruits = [];
+var HpBar;
+var MarioLook;
 
 function preload(){
 	marioImg = loadImage("Img/Mario.png");
+	marioImg1 = loadImage("Img/Mario1.png");
+	enemyImg1 = loadImage("Img/enemy1.png");
+	enemyImg2 = loadImage("Img/enemy2.png");
 }
 
 function setup() {
 	createCanvas(windowWidth,windowHeight);
 	//
 	Mario = createSprite(100,windowHeight-200,50,50);
-	Mario.addImage(marioImg);
+	Mario.addImage("right",marioImg);
+	Mario.addImage("left",marioImg1);
 	//
 	Ground = createSprite(2000/2,windowHeight-15,2000,40);
 	Ground.shapeColor = "brown";
@@ -20,69 +28,63 @@ function setup() {
 	wall1 = createSprite(-20,windowHeight/2,20,windowHeight);
 	wall1.shapeColor = "grey";
 	obs.push(wall1);
-	//
+	// The bridge
 	obs1 = createSprite(200,windowHeight-60,50,50);
 	obs1.shapeColor = "grey";
 	obs.push(obs1);
-	//
+
 	obs2 = createSprite(250,windowHeight-110,50,50);
 	obs2.shapeColor = "grey";
 	obs.push(obs2);
-	//
+
 	obs3 = createSprite(300,windowHeight-110,50,50);
 	obs3.shapeColor = "grey";
 	obs.push(obs3);
-	//
+
 	obs4 = createSprite(350,windowHeight-60,50,50);
 	obs4.shapeColor = "grey";
 	obs.push(obs4);
-	//
-	river1 = createSprite(275,windowHeight-35,100,100);
-	river1.shapeColor = "blue";
-	//
-	obs5 = createSprite(600,windowHeight-60,50,50);
-	obs5.shapeColor = "grey";
-	obs.push(obs1);
-	//
-	obs2 = createSprite(650,windowHeight-110,50,50);
-	obs2.shapeColor = "grey";
-	obs.push(obs2);
-	//
-	obs3 = createSprite(700,windowHeight-110,50,50);
-	obs3.shapeColor = "grey";
-	obs.push(obs3);
-	//
-	obs4 = createSprite(750,windowHeight-60,50,50);
-	obs4.shapeColor = "grey";
-	obs.push(obs4);
-	//
-	river = createSprite(675,windowHeight-35,100,100);
-	river.shapeColor = "blue";
+	// Enemies
+	enemy1 = new Enemy(400,windowHeight-60);
+	enemies.push(enemy1);
+
+	enemy2 = new Enemy(425,windowHeight-60);
+	enemies.push(enemy2);
 }
 
 function draw() {
 	background(0,255,255);
 	Move();
+	textSize(30);
 	
 	for(var i = 0;i<obs.length;i++){
 		Mario.collide(obs[i]);
 	}
 
+	for(var j = 0;j<enemies.length;j++){
+		if(Mario.isTouching(enemies[j])){
+			enemies[j].kill();
+		}
+	} 
+
 	drawSprites();
 
 	if(gameState == 1){
-
+		text("Use left and right arrows to move.",0,500);
+		text("Use Space to Jump.",0,550);
 	}
 }
 
 function Move(){
 	if(keyDown(RIGHT_ARROW)){
+		Mario.changeImage("right");
 		Mario.x += 4;
 	}else if(keyDown(LEFT_ARROW)){
+		Mario.changeImage("left");;
 		Mario.x -= 4;
 	}
 	if(Mario.y>700){
-		if(keyDown(32)){
+		if(keyWentDown(32)){
 			Mario.velocityY = -10;
 		}
 	}
